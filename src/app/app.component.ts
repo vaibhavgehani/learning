7f25964e6a352089e1c79f8d324225e675d1f464
vaibhav gehani <vgehani90@gmail.com>
2021-09-29 00:08:30 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
+    { title: 'Home', url: '/folder/Inbox', icon: 'home' },
+    { title: 'Learning', url: '/folder/Outbox', icon: 'book' },
+    { title: 'Discussions', url: '/folder/Favorites', icon: 'chatbubbles' },
+    { title: 'Activites', url: '/folder/Archived', icon: 'document' },
+    { title: 'Processes', url: '/folder/Trash', icon: 'shuffle' },
   ];
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {}
